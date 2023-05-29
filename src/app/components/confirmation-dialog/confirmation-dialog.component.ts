71093305967bd0ecda3d4510175a868cada286ed
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-05-29 16:46:27 -0300

    created confirmation dialog

---
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {
+      message: string | null,
+      multipleButtons: boolean,
+      buttonConfirmation: string | null,
+      buttonCancel: string | null,
+      singleButtonText: string | null
+     },
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
